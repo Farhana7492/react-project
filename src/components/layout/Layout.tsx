a411e323d4d9d0c4be4ebd0e59a38c8a178ec674
Farhana7492 <farhanaairin55@gmail.com>
2025-09-23 13:06:48 +0600

    React Project 23.09.2025

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+import Navbar from './Navbar'
+import Dashboard from '../pages/Dashboard'
+import Footer from './Footer'
+
+function Layout() {
+  return (
+    <>
+      
+    <div className="layout-wrapper layout-content-navbar">
+      <div className="layout-container">
+        
+
+        
+        {/* / Menu */}
+            <Sidebar />
+        {/* Layout container */}
+        <div className="layout-page">
+          {/* Navbar */}
+
+          <Navbar />
+
+          {/* <!-- / Navbar --> */}
+
+          {/* <!-- Content wrapper --> */}
+          <div className="content-wrapper">
+            {/* <!-- Content --> */}
+
+            <Dashboard />
+            {/* <!-- / Content --> */}
+
+            {/* <!-- Footer --> */}
+            <Footer />
+            {/* <!-- / Footer --> */}
+
+            <div className="content-backdrop fade"></div>
+          </div>
+          {/* <!-- Content wrapper --> */}
+        </div>
+        {/* <!-- / Layout page --> */}
+      </div>
+
+      {/* <!-- Overlay --> */}
+      <div className="layout-overlay layout-menu-toggle"></div>
+    </div>
+    
+    </>
+  )
+}
+
+export default Layout
